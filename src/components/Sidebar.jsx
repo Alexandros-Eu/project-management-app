3ea78179c12826e39f7a3e0a4abe87b84c102e27
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-03 18:59:37 +0200

    Added Sidebar component + tw styling

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+export default function Sidebar()
+{
+    return <aside className="bg-black float-start w-1/6 h-screen rounded-lg ps-7 pt-7">
+        <h1 className="text-white font-bold text-lg">YOUR PROJECTS</h1>
+        <button className="bg-stone-700 rounded-lg text-stone-400 px-3 py-2 mt-5 text-start">+ Add Project</button>
+    </aside>
+}
