7e579fe6804294d9b882659812cc3f065aa21cb2
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-03 19:00:12 +0200

    Added Content component + tw styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Content from './components/Content.jsx';
+import Sidebar from './components/Sidebar.jsx';
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <Sidebar/>
+      <Content/>
     </>
   );
 }
