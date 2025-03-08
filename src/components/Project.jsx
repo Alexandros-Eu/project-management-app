5702ad491dc428bf11c3f3e09dc084521a976cb5
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-08 23:23:36 +0200

    Added Project component + styling

---
+++ src/components/Project.jsx
@@ -0,0 +1,15 @@
+export default function Project({project})
+{
+    return (
+        <div className="pt-20 ps-96 pe-60">
+            <div className="flex justify-between">
+                <h1 className="text-stone-700 text-3xl font-bold">{project.title}</h1>
+                <button className="text-stone-700 font-bold">Delete</button>
+            </div>
+
+            <p className="mt-2 text-stone-600">{project.date}</p>
+            <p className="mt-2 text-stone-900">{project.description}</p>
+            <hr className="mt-4 border-t-2 border-stone-300"/>
+        </div>
+    )
+}
