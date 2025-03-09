572da11c78f6841a085b23b6c20f59163ab2d4b2
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-09 16:27:06 +0200

    Added TaskForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Content from './components/Content.jsx';
 import Sidebar from './components/Sidebar.jsx';
 import Form from './components/Form.jsx';
+import Project from './components/Project.jsx';
+import TaskForm from './components/TaskForm.jsx';
 import { useState } from 'react';
 
 function App() {
@@ -8,8 +10,8 @@ function App() {
   const [currentComponent, setCurrentComponent] = useState("defaultPage");
   const [formData, setFormData] = useState({title: "", description: "", date: ""});
   const [projectsData, setProjectsData] = useState([]);
-
-  let currentProject;
+  const [currentProject, setCurrentProject] = useState();
+  
 
   function handleInputChange(e, identifier)
   {
@@ -42,7 +44,7 @@ function App() {
   function handleProjectClick(project)
   {
       setCurrentComponent("projectRendering");
-      currentProject = project;
+      setCurrentProject(project);
   }
 
   return (
@@ -50,6 +52,8 @@ function App() {
       <Sidebar onAddProject={handleAddProject} projects={projectsData} onProjectClick={handleProjectClick}/>
       { currentComponent === "projectForm" ? <Form onFormChange={handleInputChange} formData={formData} onFormSubmit={handleFormSubmit}/> : undefined}
       { currentComponent === "defaultPage" ?  <Content onAddProject={handleAddProject}/> : undefined}
+      { currentComponent === "projectRendering" ? <Project project={currentProject}/> : undefined}
+      { currentComponent === "projectRendering" ? <TaskForm/> : undefined}
     </>
   );
 }
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,15 @@
+export default function TaskForm()
+{
+    return (
+        <form action="" className="pt-8 ps-96 pe-60">
+            <h1 className="text-stone-800 text-3xl font-bold mb-4">Tasks</h1>
+            <div>
+                <input type="text" className="bg-neutral-200 me-3 rounded"/>
+                <button className="text-zinc-900">Add Task</button>
+            </div>
+            <div className="flex justify-content-start items-center bg-neutral-200 mt-6 w-1/3 h-28 p-3 rounded-lg">
+            
+            </div>
+        </form>
+    )
+}
