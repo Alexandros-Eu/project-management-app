0af47594c5e3e90ed110c65b823e3b7fec4011ad
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-12 01:31:43 +0200

    Refactoring

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
-export default function Sidebar({onSidebarClick, projects})
+export default function Sidebar({onAddProject, projects, onProjectClick})
 {
     return <aside className="bg-black float-start w-1/6 h-screen rounded-lg ps-7 pt-7 pe-7">
         <h1 className="text-white font-bold text-lg">YOUR PROJECTS</h1>
-        <button onClick={onSidebarClick} className="bg-stone-700 rounded-lg text-stone-400 px-3 py-2 mt-5 mb-5 text-start">+ Add Project</button>
+        <button onClick={onAddProject} className="bg-stone-700 rounded-lg text-stone-400 px-3 py-2 mt-5 mb-5 text-start">+ Add Project</button>
         {projects.map((project, i) => {
             i++;
-            return <a key={i} className="block ps-3 pt-1 pb-1 text-slate-200 active:bg-zinc-900">{project.title}</a>
+            return <a key={i} onClick={() => onProjectClick(project)} className="block ps-3 pt-1 pb-1 text-slate-200 active:bg-zinc-900">{project.title}</a>
         })}
     </aside>
 }
