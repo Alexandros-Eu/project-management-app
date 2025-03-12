a46081fba8f55cab3ef151907ef966682e225076
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-12 22:51:01 +0200

    Portaled Sidebar to different part of DOM

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
+import { createPortal } from 'react-dom';
+
 export default function Sidebar({onAddProject, projects, onProjectClick})
 {
-    return <aside className="bg-black float-start w-1/6 h-screen rounded-lg ps-7 pt-7 pe-7 mt-10">
+    return createPortal(<aside className="bg-black float-start w-1/6 h-screen rounded-lg ps-7 pt-7 pe-7 mt-10">
         <div className="mt-12">
             <h1 className="text-white font-bold text-xl">YOUR PROJECTS</h1>
             <button onClick={onAddProject} className="bg-stone-700 rounded-lg text-stone-400 px-3 py-2 mt-8 mb-5 text-start">+ Add Project</button>
@@ -10,5 +12,6 @@ export default function Sidebar({onAddProject, projects, onProjectClick})
             i++;
             return <a key={i} onClick={() => onProjectClick(project)} className="block ps-3 pt-1 pb-1 text-slate-200 active:bg-zinc-900">{project.title}</a>
         })}
-    </aside>
+    </aside>,
+    document.getElementById("modal-root"))
 }
