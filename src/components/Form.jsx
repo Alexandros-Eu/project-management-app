ba67b83d2480cd7dcdf6b6719c9ccbb66bd6f8a2
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-18 23:19:16 +0200

    Minor changes in the styling of inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,13 +17,13 @@ export default function Form({onFormChange, formData, onFormSubmit, isValid})
             <div className="grid grid-cols-1 ps-12">
                 {!isValid && <p className="text-red-600">Please fill out all fields</p>}
                 <label className="mt-3 text-stone-700 font-bold" htmlFor="title">TITLE</label>
-                <input className="mt-2 bg-stone-300 h-8 text-stone-700" type="text" name="title" value={formData.title} onChange={(e) => onFormChange(e, "title")} required/>
+                <input className="mt-2 bg-stone-300 h-8 text-stone-700 w-96" type="text" name="title" value={formData.title} onChange={(e) => onFormChange(e, "title")} required/>
 
                 <label className="mt-3 text-stone-700 font-bold" htmlFor="description">DESCRIPTION</label>
-                <textarea className="mt-2 bg-stone-300 text-stone-700" name="description" id="description" value={formData.description} onChange={(e) => onFormChange(e, "description")} required></textarea>
+                <textarea className="mt-2 bg-stone-300 text-stone-700 max-w-96 h-16" name="description" id="description" value={formData.description} onChange={(e) => onFormChange(e, "description")} required></textarea>
 
                 <label className="mt-3 text-stone-700 font-bold" htmlFor="date">DUE DATE</label>
-                <input className="mt-2 bg-stone-300 text-stone-700" type="date" name="date" value={formData.date} onChange={(e) => onFormChange(e, "date")} required/>
+                <input className="mt-2 bg-stone-300 text-stone-700 w-96 h-7" type="date" name="date" value={formData.date} onChange={(e) => onFormChange(e, "date")} required/>
             </div>
         </form>
     )
