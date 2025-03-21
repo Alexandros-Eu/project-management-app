393a50f9c786f5850ef412502dd358ccac0f32db
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-21 16:36:08 +0200

    Added projectsState + content rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useState } from 'react';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject()
+  {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectID: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectID === null)
+  {
+    content = <NewProject/>
+  }
+  else if(projectsState.selectedProjectID === undefined)
+  {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
