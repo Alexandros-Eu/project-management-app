e1fe90cafb06664a65d809bc206f4a2405324b21
Alexandros-Eu <alexeouenta@gmail.com>
2025-03-27 17:34:53 +0200

    Added Input Component

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+export default function Input({label, isTextArea, data, onInputChange})
+{
+    const identifier = label;
+
+    return (
+        <>
+            <label className="mt-3 text-stone-700 font-bold uppercase" htmlFor={!isTextArea ? label : "date"}>{label}</label>
+            {!isTextArea && <input className="mt-2 p-1 bg-stone-300 h-8 text-stone-700 w-[40rem] rounded-sm border-b-2 border-stone-300 focus:outline-none focus:border-stone-600" type={label === "due date" ? "date" : "text"} name={label === "due date" ? "date" : "title"} value={data[label]} onChange={(e) => onInputChange(e, label === "due date" ? "date" : "title")} required/>}
+            {isTextArea && <textarea className="mt-2 p-1 bg-stone-300 text-stone-700 w-[40rem] h-16" name="description" id="description" value={data.description} onChange={(e) => onInputChange(e, identifier)} required></textarea>}
+        </>
+    )
+}
